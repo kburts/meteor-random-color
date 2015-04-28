2fd52fcf023c5d05f7a2e2d068da5230d5de30ee
kburts <kevin.burton2@gmail.com>
2015-04-28 16:59:15 -0700

    Shortened summary for publishing to atmosphere.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    summary: "Random Color (v0.2.0), lib for generating attractive random colors, from David Merfield, https://github.com/davidmerfield/randomColor, packaged for Meteor.js.",
+    summary: "Random Color, lib for generating attractive random colors, from David Merfield.",
     name: "kburts:randomcolor",
     version: "0.2.0",
     git: "https://github.com/rzymek/meteor-random-color.git"
