5e7df8c2424055a7628698c36515a1e2bdecf7a1
kburts <kevin.burton2@gmail.com>
2015-04-28 23:42:09 -0700

    No longer restriced to client only.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     summary: "Random Color, lib for generating attractive random colors, from David Merfield.",
     name: "kburts:randomcolor",
-    version: "0.2.0",
+    version: "0.2.1",
     git: "https://github.com/rzymek/meteor-random-color.git"
 });
 
 Package.on_use(function (api) {
-    api.add_files('lib/js/randomColor.js', 'client');
+    api.add_files('lib/js/randomColor.js');
 });
